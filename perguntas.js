40fa36fa20220530bb2b8a6f14ebbca0bdab473c
Tifoskinha <168854382+Tifoskinha@users.noreply.github.com>
2024-12-02 09:09:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Entretenimento',
+    'Em que cidade se passa a série Stranger Things?',
+    'A série se passa na cidade de Hawkins'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo?',
+    'O maior país do mundo é a Rússia'
+)
+
+criaCartao(
+    'Entretenimento',
+    'Qual é o tesouro procurado pelos personagens de Outer Banks na quarta temporada?',
+    'O tesouro procurado pelos personagens é a Coroa Azul'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz água em Inglês?',
+    'Àgua em ingles é water (uarer)'
+)
